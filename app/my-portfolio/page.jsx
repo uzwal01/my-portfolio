226f3815e07068352095510aa93f946d3aa629f7
uzwal01 <uzwalduwal07@gmail.com>
2024-12-07 11:21:43 +0545

    minor change

--- app/my-portfolio/page.jsx
+++ app/my-portfolio/page.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import portfolioImg from '/public/assets/projects/portfolio.jpg'
+import portfolioImg from '/public/assets/projects/portfolio.png'
 import {RiRadioButtonFill} from "react-icons/ri"
 import Link from 'next/link'
 const portfolio = () => {
